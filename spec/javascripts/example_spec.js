6430e0970f1c39b133daca47f1e097e2a87e76c6
Adam McLean <adam.mclean1@gmail.com>
2015-05-01 09:30:38 -0700

    add protractor.conf.js

--- spec/javascripts/example_spec.js
+++ spec/javascripts/example_spec.js
@@ -1,4 +1,4 @@
-describe('surfair homepage', function() {
+describe('angularjs homepage', function() {
     it('should greet the named user', function() {
         browser.get('http://www.angularjs.org');
 
